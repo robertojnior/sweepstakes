20bfce34daf840da899a45d2b9b26a25f7bc4a93
Roberto Júnior <robertojunior085@gmail.com>
2022-11-02 20:02:12 -0300

    create POST /pools and add x-total-count HTTP Header to GET /pools

--- pool/src/routes/pools.ts
+++ pool/src/routes/pools.ts
@@ -1,16 +1,35 @@
-import type {
-  FastifyInstance,
-  RouteShorthandOptions,
-  HookHandlerDoneFunction
-} from 'fastify'
+import type { FastifyInstance } from 'fastify'
+
+import ShortUniqueId from 'short-unique-id'
+import { z } from 'zod'
+
 import { prisma } from '../db/prisma'
 
-export function routes(
-  fastify: FastifyInstance,
-  options: RouteShorthandOptions,
-  done: HookHandlerDoneFunction
-) {
-  fastify.get('/', options, () => prisma.pool.findMany())
+export async function routes(fastify: FastifyInstance) {
+  fastify.get('/', async (_request, reply) => {
+    const pools = await prisma.pool.findMany()
 
-  done()
+    return reply.status(200).header('X-Total-Count', pools.length).send(pools)
+  })
+
+  fastify.post('/', async (request, reply) => {
+    const createPoolSchema = z.object({
+      title: z.string()
+    })
+
+    const { title } = createPoolSchema.parse(request.body)
+
+    const uniqueId = new ShortUniqueId({ length: 6 })
+
+    const code = uniqueId().toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        code,
+        title
+      }
+    })
+
+    return reply.status(201).send({ code })
+  })
 }
