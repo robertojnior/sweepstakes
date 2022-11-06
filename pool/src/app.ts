525288779e77147fd4985779b30b14461dabafce
Roberto Júnior <robertojunior085@gmail.com>
2022-11-06 15:26:03 -0300

    add fastify jwt, and createa authentication middleware

--- pool/src/app.ts
+++ pool/src/app.ts
@@ -1,10 +1,12 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { routes } from './routes/router'
 
 const app = Fastify({ logger: true })
 
+app.register(jwt, { secret: process.env.SECRET_KEY_BASE! })
 app.register(cors, { origin: true })
 app.register(routes)
 
