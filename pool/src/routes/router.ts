84e3fe245b45d8bd579016fd112dd4f3833dedb9
Roberto Júnior <robertojunior085@gmail.com>
2022-11-06 20:39:59 -0300

    add resources to router

--- pool/src/routes/router.ts
+++ pool/src/routes/router.ts
@@ -1,7 +1,17 @@
 import type { FastifyInstance } from 'fastify'
 
+import { routes as tokens } from './tokens'
+import { routes as sessions } from './sessions'
+import { routes as profile } from './profile'
+import { routes as users } from './users'
 import { routes as pools } from './pools'
+import { routes as bets } from './bets'
 
 export async function routes(fastify: FastifyInstance) {
+  fastify.register(tokens, { prefix: '/tokens' })
+  fastify.register(sessions, { prefix: '/sessions' })
+  fastify.register(profile, { prefix: '/me' })
+  fastify.register(users, { prefix: '/users' })
   fastify.register(pools, { prefix: '/pools' })
+  fastify.register(bets, { prefix: '/bets' })
 }
