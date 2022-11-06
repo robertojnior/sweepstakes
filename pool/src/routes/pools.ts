3e03b9137589d8367f781cd52c7ca49961cc69d7
Roberto Júnior <robertojunior085@gmail.com>
2022-11-06 20:44:46 -0300

    add authentication to pools resource

--- pool/src/routes/pools.ts
+++ pool/src/routes/pools.ts
@@ -4,29 +4,31 @@ import ShortUniqueId from 'short-unique-id'
 import { z } from 'zod'
 
 import { prisma } from '../db/prisma'
+import { authenticate } from '../middlewares/authenticate'
 
 export async function routes(fastify: FastifyInstance) {
   fastify.get('/', async (_request, reply) => {
     const pools = await prisma.pool.findMany()
 
-    return reply.status(200).header('X-Total-Count', pools.length).send(pools)
+    return reply.header('X-Total-Count', pools.length).send({ pools })
   })
 
-  fastify.post('/', async (request, reply) => {
+  fastify.post('/', { onRequest: authenticate }, async (request, reply) => {
     const createPoolSchema = z.object({
       title: z.string()
     })
 
     const { title } = createPoolSchema.parse(request.body)
 
-    const uniqueId = new ShortUniqueId({ length: 6 })
+    const uid = new ShortUniqueId({ length: 6 })
 
-    const code = uniqueId().toUpperCase()
+    const code = String(uid().toUpperCase())
 
     await prisma.pool.create({
       data: {
         code,
-        title
+        title,
+        ownerId: request.user.sub
       }
     })
 
